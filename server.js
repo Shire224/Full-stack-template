f875b091f613827aa0c8db9cc14746041bbc6f88
Shire224 <shire_hassen224@hotmail.com>
2022-07-15 18:10:26 -0400

    add middleware

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+const PORT = 8000
 
 let db,
     dbConnectionString = process.env.DB_STRING,
     dbName = 'sample_mflix',
-    collection 
+    collection
 
 MongoClient.connect(dbConnectionString)
     .then(client => {
         console.log(`Connected to Database`)
         db = client.db(dbName)
-        collection = db.collection('comments')
+        collection = db.collection('movies')
     })
